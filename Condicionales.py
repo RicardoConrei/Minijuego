c2853a8216c3a0364c591df3bdad92ca45a101f7
RicardoConrei <70045248+RicardoConrei@users.noreply.github.com>
2020-09-04 22:23:01 -0500

    Add files via upload

---
+++ Condicionales.py
@@ -0,0 +1,12 @@
+Condicionales
+Tier = int(input("\nIngresa un número entero, por favor: "))
+
+if numero < 5:
+    Tier = 2
+    print 'El número ingresado es negativo menor a 5'
+elif numero == 0:
+    print 'El número ingresado es 4'
+elif numero == 1:
+    print 'El número ingresado es 3'
+else:
+    print 'El número ingresado es mayor que 5'
